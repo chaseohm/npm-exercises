f3dce0a3b3b65c24602fb76ac6233fdebfe4a823
admin <chaseohm@gmail.com>
2018-12-20 13:52:45 -0600

    module exercise

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
+
+const hello = require("./say-hello.js");
 const $ = require('jquery');
 function sayHello(){
     console.log('Hello');
 }
 sayHello();
+hello();
---
+++ src/say-hello.js
@@ -0,0 +1,9 @@
+function sayHello(){
+    console.log("Welcome to the console.")
+}
+
+module.exports = sayHello;
+
+
+
+
